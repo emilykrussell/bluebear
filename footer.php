08ad76a8b82bfc3085e44fc0acb6772b7ffb07a2
Emily Russell <ekluchansky@gmail.com>
2017-04-23 18:13:26 -0400

    first commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @package Counter
+ */
+
+?>
+		</div><!-- #content -->
+	</div><!-- #main -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+		<?php get_template_part( 'template-parts/widgets-footer' ); ?>
+
+		<div class="site-info">
+		<p>Blue Bear Ice Cream, LLC</p>
+
+		</div><!-- .site-info -->
+
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
